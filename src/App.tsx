7b5bb463920cfaab4944f0ee6c569539639e43b3
laur <laurentiu@granite.ie>
2020-01-15 12:36:16 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Leeearn React
+          Learn React
         </a>
       </header>
     </div>
