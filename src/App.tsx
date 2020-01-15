ed5e7e0e871dc4cfacc72ccf874839fadde49b0b
laur <laurentiu@granite.ie>
2020-01-15 12:38:27 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Leeeearn React
+          Larn React
         </a>
       </header>
     </div>
