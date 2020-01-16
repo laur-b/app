7b4c5735e42d6545c8071486aaedf01714fb3079
laur <laurentiu@granite.ie>
2020-01-16 18:27:56 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Larn React
+          Lerttarn React
         </a>
       </header>
     </div>
