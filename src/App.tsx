9b5c6ead5e8f8a59a04edbfd45747348fddbfb13
laur <laurentiu@granite.ie>
2020-01-16 18:39:57 +0200

    fix test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Lerttarn React
+          Learn React
         </a>
       </header>
     </div>
